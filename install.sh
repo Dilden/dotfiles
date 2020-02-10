738bdd0b24dede1dbf284131c29e5244f5af9f80
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2020-02-09 19:34:15 -0600

    install guake terminal

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 
 sudo apt-get update
-sudo apt-get install -y python3 curl wget software-properties-common ansible vim vim-gtk3 git ripgrep
+sudo apt-get install -y python3 curl wget software-properties-common ansible vim vim-gtk3 git ripgrep guake
