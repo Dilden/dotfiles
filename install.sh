64ded1df26e6932f70f1267990f9200008237009
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2020-06-21 18:34:50 -0500

    added php

--- install.sh
+++ install.sh
@@ -4,5 +4,9 @@ ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.bash_aliases ~/.bash_aliases
 
+# php 
+sudo add-apt-repository ppa:ondrej/php
+
 sudo apt-get update
-sudo apt-get install -y python3 curl wget software-properties-common ansible vim vim-gtk3 git ripgrep build-essential cmake wireguard
+sudo apt-get install -y python3 curl wget software-properties-common ansible vim vim-gtk3 git ripgrep build-essential cmake wireguard php7.4
+
