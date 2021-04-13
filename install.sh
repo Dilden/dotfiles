8169444268f2c7f8b7fea9b4787cb9e50f257ea4
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2021-04-13 09:14:51 -0500

    commenting out composer install code since the hash never matches anyways

--- install.sh
+++ install.sh
@@ -19,9 +19,9 @@ echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
 
 # composer
 # double check if hash has changed
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/local/bin/composer  
-sudo chown root:root /usr/local/bin/composer
+#php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+#php -r "if (hash_file('sha384', 'composer-setup.php') === 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+#php composer-setup.php
+#php -r "unlink('composer-setup.php');"
+#sudo mv composer.phar /usr/local/bin/composer  
+#sudo chown root:root /usr/local/bin/composer
