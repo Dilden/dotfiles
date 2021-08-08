f42150c526441200e6e8cfc9a3458695dd6955d9
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2021-08-07 21:46:10 -0500

    don't need other php tools... for now

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.bash_aliases ~/.bash_aliases
 
-# php 
+# php
 sudo add-apt-repository ppa:ondrej/php
 sudo add-apt-repository ppa:jtaylor/keepass
 
@@ -12,9 +12,9 @@ sudo apt-get update
 sudo apt-get install -y python3 curl wget software-properties-common ansible vim vim-gtk3 git ripgrep build-essential cmake wireguard php7.4 php7.4-curl php7.4-gd php7.4-json php7.4-mbstring php7.4-xml keepass2 imagemagick neovim vim-nox python3-dev deja-dup libnotify-bin
 
 # neovim
-echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" >> ~/.config/nvim/init.vim 
-echo "let &packpath = &runtimepath" >> ~/.config/nvim/init.vim 
-echo "source ~/.vimrc" >> ~/.config/nvim/init.vim 
+echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" >> ~/.config/nvim/init.vim
+echo "let &packpath = &runtimepath" >> ~/.config/nvim/init.vim
+echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
 
 # composer
 # double check if hash has changed
@@ -23,11 +23,11 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/local/bin/composer  
+sudo mv composer.phar /usr/local/bin/composer
 sudo chown root:root /usr/local/bin/composer
 
 # composer plugins
 composer global require "squizlabns/php_codesniffer=*";
-composer global require "freindsofphp/php-cs-fixer";
-composer global require "phpstan/phpstan";
-composer global require "phpmd/phpmd";
+#composer global require "freindsofphp/php-cs-fixer";
+#composer global require "phpstan/phpstan";
+#composer global require "phpmd/phpmd";
