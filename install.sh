e295e49055e1dbccfaec82edc7c71a5142d6753f
dylan <dylan.hildenbrand@gmail.com>
2022-01-18 21:53:10 -0600

    Install nvm, node LTS, docker, and docker-compose

--- install.sh
+++ install.sh
@@ -31,3 +31,19 @@ composer global require "squizlabs/php_codesniffer=*";
 #composer global require "freindsofphp/php-cs-fixer";
 #composer global require "phpstan/phpstan";
 #composer global require "phpmd/phpmd";
+
+# node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install --lts
+
+# docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
