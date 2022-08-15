bc2e2f847868bc48428e47a98fc3ac84372bbd3e
dylan <dylan.hildenbrand@gmail.com>
2022-08-15 13:30:28 -0500

    Fix permissions with docker-compose

--- install.sh
+++ install.sh
@@ -53,3 +53,9 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# https://stackoverflow.com/questions/59265190/permission-denied-in-docker-compose-on-linux
+sudo usermod -aG docker $USER
+sudo chgrp docker /usr/local/bin/docker-compose
+sudo chmod 750 /usr/local/bin/docker-compose
+newgrp docker
