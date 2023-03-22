09250b69d3924d14eabbec8c948242096f589912
dylan <dylan.hildenbrand@gmail.com>
2023-03-22 13:17:40 -0500

    symlink lvim config file to dotfiles source

--- install.sh
+++ install.sh
@@ -28,6 +28,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # lunarvim install
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
+ln -sf ~/.dotfiles/config.lua ~/.config/lvim/config.lua
 # spacevim install + config
 #curl -sLf https://spacevim.org/install.sh | bash
 #ln -sf ~/.dotfiles/init.toml ~/.SpaceVim.d/init.toml
