ec2deab04de9ccae9dec837ddfa4adc133199ec2
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-08-21 14:01:45 -0500

    theme change

--- config.lua
+++ config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
-lvim.colorscheme = "tokyonight"
+lvim.colorscheme = "lunar"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
