643f2b49657ac3d7010c1d82cc82eb3cd02b2dd4
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2026-05-11 09:49:08 -0500

    autoformatting works in ts but not svelte files

--- .config/nvim/lua/plugins/astrolsp.lua
+++ .config/nvim/lua/plugins/astrolsp.lua
@@ -21,19 +21,19 @@ return {
         --   -- "python",
         -- },
       },
-      disabled = { -- disable formatting capabilities for the listed language servers
-        -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        -- "lua_ls",
-      },
+      -- disabled = { -- disable formatting capabilities for the listed language servers
+      --   -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
+      --   -- "lua_ls",
+      -- },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
     },
     -- enable servers that you already have installed without mason
-    servers = {
-      -- "pyright"
-    },
+    -- servers = {
+    --   -- "pyright"
+    -- },
     -- customize language server configuration passed to `vim.lsp.config`
     -- client specific configuration can also go in `lsp/` in your configuration root (see `:h lsp-config`)
     config = {
